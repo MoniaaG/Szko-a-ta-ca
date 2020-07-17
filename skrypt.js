7c91515cdbc0da43671c6bfae11afc94f017abe8
MoniaaG <gormon1f@gmail.com>
2020-07-17 11:19:37 +0200

    Pełna wesja projektu

---
+++ skrypt.js
@@ -0,0 +1,38 @@
+function zmientlo1()
+{
+
+     document.getElementById("srodek2").style.backgroundColor="#eeeeee";
+
+}
+
+function przywroctlo1()
+{
+
+    document.getElementById("srodek2").style.backgroundColor="#ffffff";
+}
+
+function zmientlo2()
+{
+
+     document.getElementById("srodek").style.backgroundColor="#4a535b";
+
+}
+
+function przywroctlo2()
+{
+
+    document.getElementById("srodek").style.backgroundColor="#5b646c";
+}
+
+function zmientlo3()
+{
+
+     document.getElementById("tekstlewy").style.backgroundColor="#232a30";
+
+}
+
+function przywroctlo3()
+{
+
+    document.getElementById("tekstlewy").style.backgroundColor="#343a40";
+}
